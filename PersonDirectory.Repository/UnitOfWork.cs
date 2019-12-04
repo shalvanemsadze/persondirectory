f6d71d87dd9933ca5ed52fa0572b8d4e7d8c518e
Shalva <Shalvanemsadze@gmail.com>
2019-12-04 12:37:12 +0400

    uow few changes

--- PersonDirectory.Repository/UnitOfWork.cs
+++ PersonDirectory.Repository/UnitOfWork.cs
@@ -7,46 +7,46 @@ namespace PersonDirectory.Repository
 {
     public class UnitOfWork : IDisposable
     {
-        private PersonDirectoryContext Context = null;
-        private Dictionary<Type, object> repositories = null;
+        private PersonDirectoryContext _context = null;
+        private Dictionary<Type, object> _repositories = null;
 
         public UnitOfWork(PersonDirectoryContext context)
         {
-            Context = context;
-            repositories = new Dictionary<Type, object>(); 
+            _context = context;
+            _repositories = new Dictionary<Type, object>(); 
         }
 
         public TIRepository Repository<TIRepository,TRepository>() where TIRepository : class  
         {
-            if (repositories.Keys.Contains(typeof(TIRepository)))
-                return (TIRepository)repositories[typeof(TIRepository)];
+            if (_repositories.Keys.Contains(typeof(TIRepository)))
+                return (TIRepository)_repositories[typeof(TIRepository)];
 
-            TRepository repository = (TRepository)Activator.CreateInstance(typeof(TRepository), args: Context);
-            repositories.Add(typeof(TIRepository), repository);
-            return (TIRepository)repositories[typeof(TIRepository)];
+            TRepository repository = (TRepository)Activator.CreateInstance(typeof(TRepository), args: _context);
+            _repositories.Add(typeof(TIRepository), repository);
+            return (TIRepository)_repositories[typeof(TIRepository)];
         }
 
         public void SaveChanges()
         {
-            this.Context.SaveChanges();
+            _context.SaveChanges();
         }
 
         private bool disposed = false;
         protected virtual void Dispose(bool disposing)
         {
-            if (!this.disposed)
+            if (!disposed)
             {
                 if (disposing)
                 {
-                    Context.Dispose();
+                    _context.Dispose();
                 }
             }
-            this.disposed = true;
+            disposed = true;
         }
 
         public void Dispose()
         {
-            this.Dispose(true);
+            Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
